f22275677f6ea76ef313a2712453001e90e286b1
Menios Sin <menios.sin@gmail.com>
2019-02-01 16:39:42 +0200

    This is the first message for the QuickPoll.Fixed dependencies pom.xml

--- src/main/java/com/apress/repository/OptionRepository.java
+++ src/main/java/com/apress/repository/OptionRepository.java
@@ -1,9 +1,12 @@
 package com.apress.repository;
 
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
+
+import com.apress.domain.Vote;
 
 // Because the OptionRepository works with the Option domain object
 // it passes Option and Long as generic parameter values
 @Repository
-public interface OptionRepository {
+public interface OptionRepository extends CrudRepository<Vote, Long> {
 }
